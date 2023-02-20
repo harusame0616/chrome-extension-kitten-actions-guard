da9d166c7c37e913d91192229e2fb5ee1880726c
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-02-21 06:02:57 +0900

    eslint 設定追加

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["import", "@typescript-eslint/eslint-plugin"],
+  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended", "prettier"],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      " @typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "tsconfig.json",
+      },
+    },
+  },
+};
